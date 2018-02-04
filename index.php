f4a6e3dfedfa0fee5507b1da1910fae51a457bb2
maashey <mbalueva@gmail.com>
2018-02-04 23:21:02 +0300

    Добавила массивы для категорий и списка объявлений и циклы для их прохода

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL); 
+
 $is_auth = (bool) rand(0, 1);
 
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
+
+//список категорий
+$categories= ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+
+//записываем в ассоциативный массив данные объявления
+function item_to_arr($name, $cat, $price, $img) {
+    return array('name'=> $name, 'category'=> $cat, 'price'=> $price, 'img'=> $img);
+}
+
+//список объявлений
+$items=[];
+$items[]= item_to_arr('2014 Rossignol District Snowboard','Доски и лыжи', 10999,'img/lot-1.jpg');
+$items[]= item_to_arr('DC Ply Mens 2016/2017 Snowboard','Доски и лыжи', 159999,'img/lot-2.jpg');
+$items[]= item_to_arr('Крепления Union Contact Pro 2015 года размер L/XL','Крепления', 8000,'img/lot-3.jpg');
+$items[]= item_to_arr('Ботинки для сноуборда DC Mutiny Charocal','Ботинки', 10999,'img/lot-4.jpg');
+$items[]= item_to_arr('Куртка для сноуборда DC Mutiny Charocal','Одежда', 7500,'img/lot-5.jpg');
+$items[]= item_to_arr('Маска Oakley Canopy','Разное',5400 ,'img/lot-6.jpg');
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -28,7 +49,7 @@ $user_avatar = 'img/user.jpg';
 
         <nav class="user-menu">
 
-        <? if ($is_auth): ?>
+        <? if ($is_auth) { ?>
              <div class="user-menu__image">
                  <img src="<?=$user_avatar ;?>" width="40" height="40" alt="Пользователь">
              </div>
@@ -36,7 +57,7 @@ $user_avatar = 'img/user.jpg';
                  <p><?=$user_name ;?></p>
                  <a href="#">Выйти</a>
              </div>
-         <? else: ?>    
+         <? }else{ ?>    
              <ul class="user-menu__list">
                  <li class="user-menu__item">
                      <a href="#">Регистрация</a>
@@ -45,7 +66,7 @@ $user_avatar = 'img/user.jpg';
                      <a href="#">Вход</a>
                  </li>
              </ul>
-         <? endif; ?>
+         <? } ?>
 
         </nav>
     </div>
@@ -81,24 +102,26 @@ $user_avatar = 'img/user.jpg';
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+            <? foreach($items as $item) { ?>
+                    <li class="lots__item lot">
+                        <div class="lot__image">
+                            <img src="<?= $item['img']; ?>" width="350" height="260" alt="<?= $item['name']; ?>">
                         </div>
-                        <div class="lot__timer timer">
+                        <div class="lot__info">
+                            <span class="lot__category"><?= $item['category']; ?></span>
+                            <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $item['name']; ?></a></h3>
+                            <div class="lot__state">
+                                <div class="lot__rate">
+                                    <span class="lot__amount">Стартовая цена</span>
+                                    <span class="lot__cost"><?= number_format($item['price'], 0, '', ' ') ; ?><b class="rub">р</b></span>
+                                </div>
+                                <div class="lot__timer timer">
 
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </li>
+                    </li>
+            <? } ?>    
         </ul>
     </section>
 </main>
@@ -106,24 +129,11 @@ $user_avatar = 'img/user.jpg';
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <li class="nav__item">
-                <a href="all-lots.html">Доски и лыжи</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Крепления</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Одежда</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Разное</a>
-            </li>
+            <? foreach ($categories as $category) { ?>
+                <li class="nav__item">
+                    <a href="all-lots.html"><?= $category; ?></a>
+                </li>
+            <? } ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
