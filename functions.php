eb63cdfd8b3a2c18a2dd18578e49b71911ef44dc
maashey <mbalueva@gmail.com>
2018-02-10 13:18:42 +0300

    перенесла все функции в functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,22 @@
 <?php
+
+//записываем в ассоциативный массив данные объявления
+function item_to_arr($name, $cat, $price, $img) {
+    return array('name'=> $name, 'category'=> $cat, 'price'=> $price, 'img'=> $img);
+}
+
+
+//форматирование вывода цены 
+function format_price($price) {
+    $result=floatval($price); //переводим в число на всякий случай
+    $result=ceil($result); //округляем до целого
+    $result= number_format($result, 0, '', ' ');
+    //$result.= ' &#8381;'; //знак рубля у меня в браузере не работает. Знак подставляется картинкой в вёрстке lot__cost
+    return $result;
+}
+
+
+//функция шаблонизатор
 function render_template($template_path, $vars)
 {
 		//Проверяем, что файл шаблона, переданный в аргументе, существует. Если не существует, то функция вернет пустую строку
--- index.php
+++ index.php
@@ -12,11 +12,6 @@ $user_avatar = 'img/user.jpg';
 //список категорий
 $categories= ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
 
-//записываем в ассоциативный массив данные объявления
-function item_to_arr($name, $cat, $price, $img) {
-    return array('name'=> $name, 'category'=> $cat, 'price'=> $price, 'img'=> $img);
-}
-
 //список объявлений
 $items=[];
 $items[]= item_to_arr('2014 Rossignol District Snowboard','Доски и лыжи', 10999,'img/lot-1.jpg');
@@ -25,14 +20,5 @@ $items[]= item_to_arr('Крепления Union Contact Pro 2015 года раз
 $items[]= item_to_arr('Ботинки для сноуборда DC Mutiny Charocal','Ботинки', 10999,'img/lot-4.jpg');
 $items[]= item_to_arr('Куртка для сноуборда DC Mutiny Charocal','Одежда', 7500,'img/lot-5.jpg');
 $items[]= item_to_arr('Маска Oakley Canopy','Разное',5400 ,'img/lot-6.jpg');
-
-//форматирование вывода цены 
-function format_price($price) {
-    $result=floatval($price); //переводим в число на всякий случай
-    $result=ceil($result); //округляем до целого
-    $result= number_format($result, 0, '', ' ');
-    //$result.= ' &#8381;'; //знак рубля у меня в браузере не работает. Знак подставляется картинкой в вёрстке lot__cost
-    return $result;
-}
 
 ?>
