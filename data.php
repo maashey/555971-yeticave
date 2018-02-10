6a664c3f8b07115bbb6ac2d15edc8c4520d8474f
maashey <mbalueva@gmail.com>
2018-02-10 13:21:08 +0300

    перенесла данные в data.php и подключила его в index.php

--- data.php
+++ data.php
@@ -6,3 +6,20 @@ $bets = [
     ['name' => 'Евгений', 'price' => 10500, 'ts' => strtotime('-' . rand(25, 50) .' hour')],
     ['name' => 'Семён', 'price' => 10000, 'ts' => strtotime('last week')]
 ];
+
+$is_auth = (bool) rand(0, 1);
+
+$user_name = 'Константин';
+$user_avatar = 'img/user.jpg';
+
+//список категорий
+$categories= ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+
+//список объявлений
+$items=[];
+$items[]= item_to_arr('2014 Rossignol District Snowboard','Доски и лыжи', 10999,'img/lot-1.jpg');
+$items[]= item_to_arr('DC Ply Mens 2016/2017 Snowboard','Доски и лыжи', 159999,'img/lot-2.jpg');
+$items[]= item_to_arr('Крепления Union Contact Pro 2015 года размер L/XL','Крепления', 8000,'img/lot-3.jpg');
+$items[]= item_to_arr('Ботинки для сноуборда DC Mutiny Charocal','Ботинки', 10999,'img/lot-4.jpg');
+$items[]= item_to_arr('Куртка для сноуборда DC Mutiny Charocal','Одежда', 7500,'img/lot-5.jpg');
+$items[]= item_to_arr('Маска Oakley Canopy','Разное',5400 ,'img/lot-6.jpg');
--- index.php
+++ index.php
@@ -3,22 +3,6 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL); 
 
 require_once('functions.php');
-
-$is_auth = (bool) rand(0, 1);
-
-$user_name = 'Константин';
-$user_avatar = 'img/user.jpg';
-
-//список категорий
-$categories= ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
-
-//список объявлений
-$items=[];
-$items[]= item_to_arr('2014 Rossignol District Snowboard','Доски и лыжи', 10999,'img/lot-1.jpg');
-$items[]= item_to_arr('DC Ply Mens 2016/2017 Snowboard','Доски и лыжи', 159999,'img/lot-2.jpg');
-$items[]= item_to_arr('Крепления Union Contact Pro 2015 года размер L/XL','Крепления', 8000,'img/lot-3.jpg');
-$items[]= item_to_arr('Ботинки для сноуборда DC Mutiny Charocal','Ботинки', 10999,'img/lot-4.jpg');
-$items[]= item_to_arr('Куртка для сноуборда DC Mutiny Charocal','Одежда', 7500,'img/lot-5.jpg');
-$items[]= item_to_arr('Маска Oakley Canopy','Разное',5400 ,'img/lot-6.jpg');
+require_once('data.php');
 
 ?>
