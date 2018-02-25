b0ffb8859832c460670b73b0862c5c4026a20d36
maashey <mbalueva@gmail.com>
2018-02-25 18:18:48 +0300

    История просмотров via cookies

---
+++ history.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once('functions.php');
+require_once('data.php');
+
+if (isset($_COOKIE['visited_lots'])) {
+    $visited_lots = json_decode($_COOKIE['visited_lots']);
+}
+else {
+    $visited_lots = [];
+}
+
+$content =  render_template( 'view_history', ['lots' => $lots, 'expiration' => $expiration, 'visited_lots' => $visited_lots]);
+
+render_page($content, 'История просмотров', $categories, $is_auth, $user_name, $user_avatar);
--- lot.php
+++ lot.php
@@ -9,21 +9,27 @@ require_once('data.php');
 
 $lot = null;
 
+if (isset($_COOKIE['visited_lots'])) {
+    $visited_lots = json_decode($_COOKIE['visited_lots']);
+}
+else {
+    $visited_lots = [];
+}
+
 if (isset($_GET['lot_id'])) {
 	$lot_id = $_GET['lot_id'];
 
-	foreach ($lots as $item) {
-		if ($item['id'] == $lot_id) {
-			$lot = $item;
-			break;
-		}
-	}
+    $lot = $lots[$lot_id] ?? null;
+    if(!in_array($lot_id, $visited_lots)) {
+        array_push($visited_lots, $lot_id);
+        setcookie('visited_lots', json_encode($visited_lots), strtotime("+30 days"));
+    }
 }
 
 if (!$lot) {
 	http_response_code(404);
 }
 
-$content = render_template('view_lot', ['lot' => $lot, 'bets'=> $bets, 'expiration' => $expiration]);
+$content = render_template('view_lot', ['lot' => $lot, 'bets' => $bets, 'expiration' => $expiration]);
 
 render_page($content, esc($lot['name']), $categories, $is_auth, $user_name, $user_avatar);
