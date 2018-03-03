d805f9ebd13411552c0ea8267060dc3f83178e98
maashey <mbalueva@gmail.com>
2018-03-04 02:15:50 +0300

    история просмотров

--- history.php
+++ history.php
@@ -1,7 +1,7 @@
 <?php
 require_once('error_reporting.php');
 require_once('functions.php');
-require_once('data.php');
+require_once('db_connect.php');
 
 session_start();
 
@@ -11,7 +11,25 @@ if (isset($_COOKIE['visited_lots'])) {
 else {
     $visited_lots = [];
 }
+$query = 'SELECT lots.id, lots.name, lots.img_path, lots.price, lots.expiration, cat.name as category
+    FROM lots 
+    JOIN categories cat ON lots.category_id = cat.id 
+    WHERE lots.id IN (' . implode(",", $visited_lots) . ')';
+if ($res = mysqli_query($db, $query)) {
+    $lots_arr = mysqli_fetch_all($res, MYSQLI_ASSOC);
+    $lots=[];
+    foreach ($visited_lots as $index){
+        foreach ($lots_arr as $lot){
+            if($lot['id'] == $index){
+                $lots[]=$lot;
+            }
+        }
+    }
 
-$content =  render_template( 'view_history', ['lots' => $lots, 'expiration' => $expiration, 'visited_lots' => $visited_lots]);
+    $content = render_template('view_history', ['lots' => array_reverse($lots)] );
+}
+else {
+    $content = render_template('error', ['error' => mysqli_error($db)]);
+}
 
 render_page($content, 'История просмотров', $categories );
--- templates/view_history.php
+++ templates/view_history.php
@@ -2,23 +2,22 @@
     <section class="lots">
         <h2>История просмотров</h2>
         <ul class="lots__list">
-            <? foreach($visited_lots as $index) {
-                $lot = $lots[$index]; ?>
+            <? foreach($lots as $lot) { ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?=esc($lot['img']); ?>" width="350" height="260"
+                        <img src="<?=esc($lot['img_path']); ?>" width="350" height="260"
                              alt="<?=esc($lot['name']); ?>">
                     </div>
                     <div class="lot__info">
                         <span class="lot__category"><?=esc($lot['category']); ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="lot.php?lot_id=<?=$index; ?>"><?=esc($lot['name']); ?></a></h3>
+                        <h3 class="lot__title"><a class="text-link" href="lot.php?lot_id=<?=$lot['id']; ?>"><?=esc($lot['name']); ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
                                 <span class="lot__cost"><?=format_price($lot['price']); ?><b class="rub">р</b></span>
                             </div>
                             <div class="lot__timer timer">
-                                <?= $expiration; ?>
+                                <?= $lot['expiration']; ?>
                             </div>
                         </div>
                     </div>
